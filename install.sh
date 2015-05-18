64e9a0ade594adbd17e545fcb1054b1906d68d8b
DanielFGray <DanielFGray@gmail.com>
2015-05-18 14:25:05 -0500

    install.sh update

--- install.sh
+++ install.sh
@@ -4,11 +4,46 @@ cd "${BASH_SOURCE%/*}"
 thisdir="$PWD"
 
 has() {
+	if [[ $1 == '-s' ]]; then
+		silent=true
+		shift
+	fi
 	if type "$1" &> /dev/null; then
 		return 0
 	else
-		[[ ! "$2" == '-s' ]] && echo >&2 "$1 not found"
+		[[ "$silent" != true ]] && err "$1 not found"
 		return 1
+	fi
+}
+
+err() {
+	if [[ -t 0 ]]; then
+		echo >&2 "$*"
+	else
+		zenity --error --text="$*" 2> /dev/null
+	fi
+}
+
+msg() {
+	if [[ -t 0 ]]; then
+		echo "$*"
+	else
+		zenity --info --text="$*" 2> /dev/null
+	fi
+}
+
+ask() {
+	if [[ -t 0 ]]; then
+		prompt='Y/n'
+		read -n1 -p "$* [$prompt] " ans
+		echo ''
+		if [[ ${ans^} == Y* ]]; then
+			return 0
+		else
+			return 1
+		fi
+	else
+		zenity --question --text="$*" 2> /dev/null
 	fi
 }
 
@@ -21,37 +56,16 @@ backup_then_symlink() {
 	done
 }
 
-prompt() {
-	if [[ -z $2 || ${2^} = Y* ]]; then
-		prompt='Y/n'
-		default='Y'
-	elif [[ ${2^} = N* ]]; then
-		prompt='y/N'
-		default='N'
-	fi
-	while true; do
-		read -p "$1 [$prompt] " ans
-		if [[ -z $ans ]]; then
-			ans=$default
-		fi
-		if [[ ${ans^} = Y* ]]; then
-			return 0
-		elif [[ ${ans^} = N* ]]; then
-			return 1
-		fi
-	done
-}
-
-if ! has 'git' -s || ! has 'curl' -s; then
-	echo >&2 'git and curl both required'
+if ! has -s 'git' || ! has -s 'curl'; then
+	err 'git and curl both required'
 	exit 1
 fi
 
-if prompt 'symlink profile and bash_aliases?'; then
+if ask 'symlink profile and bash_aliases?'; then
 	backup_then_symlink 'profile' 'bash_aliases'
 fi
 
-if has 'vim' && prompt 'install vim plugins?'; then
+if has 'vim' && ask 'install vim plugins?'; then
 	mkdir -vp ${HOME}/.vim/{autoload,bundle,cache,undo,backups,swaps}
 	if [[ ! -f ${HOME}/.vim/autoload/plug.vim ]]; then
 		curl -fLo ${HOME}/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -59,7 +73,7 @@ if has 'vim' && prompt 'install vim plugins?'; then
 	backup_then_symlink 'vimrc'
 fi
 
-if has 'zsh' && prompt 'git clone oh-my-zsh and plugins?'; then
+if has 'zsh' && ask 'git clone oh-my-zsh and plugins?'; then
 	if [[ ! -d "${HOME}/.oh-my-zsh" ]]; then
 		git clone https://github.com/robbyrussell/oh-my-zsh.git "${HOME}/.oh-my-zsh"
 	fi
@@ -70,31 +84,31 @@ if has 'zsh' && prompt 'git clone oh-my-zsh and plugins?'; then
 	backup_then_symlink 'zshrc' 'zprofile' 'zlogin'
 fi
 
-if has 'tmux' && prompt 'symlink tmux.conf and install plugins?'; then
+if has 'tmux' && ask 'symlink tmux.conf and install plugins?'; then
 	if [[ ! -d "${HOME}/.tmux/plugins/tpm" ]]; then
 		mkdir -vp "${HOME}/.tmux/plugins"
 		git clone https://github.com/tmux-plugins/tpm "${HOME}/.tmux/plugins/tpm"
 	fi
 	[[ -f "${HOME}/.tmux.conf" ]] && mv "${HOME}/.tmux.conf" "${HOME}/old.tmux.conf"
-	if prompt 'use remote tmux conf?'; then
+	if ask 'use remote tmux conf?'; then
 		ln -vs "${thisdir}/remote.tmux.conf" "${HOME}/.tmux.conf"
 	else
 		ln -vs "${thisdir}/local.tmux.conf" "${HOME}/.tmux.conf"
 	fi
 fi
 
-if has 'xmodmap' && prompt 'symlink xmodmap?'; then
+if has 'xmodmap' && ask 'symlink xmodmap?'; then
 	backup_then_symlink 'xmodmap'
 	xmodmap ~/.xmodmap
 fi
 
-if has 'xrdb' && prompt 'symlink Xresources?'; then
+if has 'xrdb' && ask 'symlink Xresources?'; then
 	backup_then_symlink 'Xresources'
 	xrdb -load ~/.Xresources
 fi
 
-if has 'fc-cache' && prompt 'install tewi?'; then
-	mkdir -p ~/build
+if has 'fc-cache' && ask 'install tewi?'; then
+	mkdir -p ~/build ~/.fonts
 	cd ~/build
 	if [[ ! -d "${HOME}/build/tewi-font" ]]; then
 		git clone https://github.com/lucy/tewi-font
@@ -106,4 +120,5 @@ if has 'fc-cache' && prompt 'install tewi?'; then
 	make
 	cp *.pcf ~/.fonts
 	mkfontdir ~/.fonts; xset +fp ~/.fonts ; xset fp rehash; fc-cache -f
+	cd "$thisdir"
 fi
