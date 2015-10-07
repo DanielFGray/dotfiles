3d81e7754b6f5196c18e50521763b8d95a6ba13a
DanielFGray <DanielFGray@gmail.com>
2015-10-07 15:15:00 -0500

    install script updated

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd "${BASH_SOURCE%/*}"
+cd "${BASH_SOURCE%/*}" || exit
 declare thisdir="$PWD"
 declare verbose
 
@@ -29,7 +29,7 @@ backup_then_symlink() {
 library() {
 	url="$1"
 	path="$2"
-	if [[ -d "${path}" ]]; then
+	if [[ -d "$path" ]]; then
 		if [[ -d "${path}/.git" ]]; then
 			echo "git -C ${path} pull"
 			git -C "$path" pull
@@ -38,8 +38,8 @@ library() {
 		fi
 	fi
 	if [[ ! -d "$path" ]]; then
-		mkdir -p "${path%/}"
-		git clone "$url" "${path}"
+		mkdir -p ${verbose:+-v} "${path%/*}"
+		git clone "$url" "$path"
 	fi
 }
 
@@ -49,48 +49,53 @@ if ! has 'git' || ! has 'curl'; then
 fi
 
 if ask 'symlink profile and bash_{aliases,utils}?'; then
-	backup_then_symlink 'profile' 'bash_aliases' 'bash_utils'
+	backup_then_symlink profile bash_aliases bash_utils
 fi
 
-if has 'vim' && ask 'install vimrc?'; then
-	backup_then_symlink 'vimrc'
+if has vim && ask 'install vimrc?'; then
+	backup_then_symlink vimrc
 fi
 
-if has 'zsh' && ask 'git clone oh-my-zsh and plugins?'; then
-	library 'https://github.com/robbyrussell/oh-my-zsh.git' "${HOME}/.oh-my-zsh"
-	library 'https://github.com/zsh-users/zsh-syntax-highlighting.git' "${HOME}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+if has zsh && ask 'git clone oh-my-zsh and plugins?'; then
+	library https://github.com/robbyrussell/oh-my-zsh.git ${HOME}/.oh-my-zsh
+	library https://github.com/zsh-users/zsh-syntax-highlighting.git ${HOME}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+	backup_then_symlink zshrc zprofile zlogin
 fi
 
-if has 'tmux' && ask 'symlink tmux.conf and tpm?'; then
-	backup_then_symlink 'tmux.conf'
-	library 'https://github.com/tmux-plugins/tpm' "${HOME}/.tmux/plugins/tpm"
+if has tmux && ask 'symlink tmux.conf and tpm?'; then
+	backup_then_symlink tmux.conf
+	library https://github.com/tmux-plugins/tpm ${HOME}/.tmux/plugins/tpm
 fi
 
-if ask 'symlink gitconfig {gem,npm}rc?'; then
-	backup_then_symlink 'gitconfig' 'npmrc' 'gemrc'
+if ask 'symlink inputrc gitconfig {gem,npm}rc?'; then
+	backup_then_symlink inputrc gitconfig npmrc gemrc
 fi
 
-if has 'X'; then
-	if has 'xmodmap' && ask 'symlink xmodmap?'; then
-		backup_then_symlink 'xmodmap'
-		xmodmap ~/.xmodmap
+if has X; then
+	export DISPLAY=:0
+
+	if has xmodmap && ask 'install and run xmodmap?'; then
+		backup_then_symlink xmodmap
+		xmodmap ${HOME}/.xmodmap
 	fi
 
-	if has 'xrdb' && ask 'symlink Xresources?'; then
-		backup_then_symlink 'Xresources'
-		xrdb -load ~/.Xresources
+	if has xrdb && ask 'symlink Xresources?'; then
+		backup_then_symlink Xresources
+		xrdb -load ${HOME}/.Xresources
 	fi
 
-	if has 'fc-cache' && ask 'install tewi?'; then
-		mkdir ${verbose:+-v} -p ~/build ~/.fonts
-		cd ~/build
-		library 'https://github.com/lucy/tewi-font' "${HOME}/build/tewi-font"
-		cd ~/build/tewi-font
+	if has fc-cache && ask 'install fonts?'; then
+		mkdir ${verbose:+-v} -p ${HOME}/{build,.fonts}
+		library https://github.com/lucy/tewi-font ${HOME}/build/tewi-font
+		cd ${HOME}/build/tewi-font || die 'could not cd'
 		make
-		cp ${verbose:+-v} *.pcf ~/.fonts
-		mkfontdir ~/.fonts; xset +fp ~/.fonts ; xset fp rehash; fc-cache -f
+		cp ${verbose:+-v} ./*.pcf ${HOME}/.fonts
+		unzip ${thisdir}/../downloads/fantasque-sans-mono.zip '*.ttf' -d ~/.fonts
+		mkfontdir ${HOME}/.fonts
+		xset +fp ${HOME}/.fonts
+		xset fp rehash
+		fc-cache -f ${verbose:+-v}
 		cd "$thisdir"
 	fi
-fi
 
-info 'done!'
+fi
