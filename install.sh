c5b3acad19a9ab03e885722fd22237ca06987e8d
DanielFGray <DanielFGray@gmail.com>
2018-04-24 14:17:25 -0500

    update install

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ declare verbose
 declare -a errors
 declare -a configs_avail
 declare -a configs_chosen
+declare -a args
+declare process=4
 
 source "./bash_utils"
 
@@ -27,7 +29,11 @@ curl() {
 }
 export -f curl
 
-backup_then_symlink() {
+mkd() {
+  mkdir ${verbose:+-v} -p "$@" || die 'failed to mkdir $*'
+}
+
+install_dots() {
   local f src dest
   for f; do
     src=$(realpath "${thisdir}/${f}")
@@ -40,14 +46,14 @@ backup_then_symlink() {
       echo_cmd mv "$dest" "${dest%/*}/old-${dest##*/}"
     fi
     mkdir -p "${dest%/*}/"
-    echo_cmd ln -sf "$src" "$dest"
+    echo_cmd ln ${verbose:+-v} -s "$src" "$dest"
   done
 }
 
 library() {
   local repo path flags
   flags=()
-  if (( $# != 2 )); then
+  if (( $# < 2 )); then
     err 'library() needs repo and path to clone to'
     return 1
   fi
@@ -56,6 +62,8 @@ library() {
     *) repo="ssh://git@github.com/danielfgray/$1" ;;
   esac
   path="$2"
+  shift 2
+  flags+=( "$@" )
   if [[ -d "$path" ]]; then
     if [[ -d "${path}/.git" ]]; then
       echo_cmd git -C "$path" pull
@@ -64,13 +72,13 @@ library() {
     fi
   fi
   if [[ ! -d "$path" ]]; then
-    mkdir ${verbose:+-v} -p "${path%/*}"
+    mkd "${path%/*}"
     echo_cmd git clone "${flags[@]}" "$repo" "$path"
   fi
 }
 
 config_base() {
-  backup_then_symlink profile bashrc bash_aliases bash_utils inputrc gitconfig
+  install_dots profile bashrc bash_aliases bash_utils inputrc gitconfig
 }
 
 config_scripts() {
@@ -80,23 +88,35 @@ config_scripts() {
   library tekup ~/build/tekup
   library yaxg ~/build/yaxg
   library boiler ~/build/boiler
+  mkd ~/.local/bin
   find ~/build/{yaxg,boiler,tekup,fzf-scripts,api-helper} -maxdepth 1 -executable -type f -exec ln ${verbose:+-v} -s -t "$HOME/.local/bin" {} \;
 }
 
-has -v vim && config_vim() {
-  backup_then_symlink vimrc
+has vim && config_vim() {
+  install_dots vimrc
   mkdir ${verbose:+-v} -p ~/.vim/{autoload,bundle,cache,undo,backups,swaps}
   curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  vim +PlugInstall +qa
 }
 
-has -v zsh && config_zsh() {
-  backup_then_symlink zsh zshrc zshenv zlogin
-  library https://github.com/zdharma/fast-syntax-highlighting ~/.zsh/plugins/fast-syntax-highlighting
-  library https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/plugins/zsh-autosuggestions
-  library https://github.com/hlissner/zsh-autopair ~/.zsh/plugins/zsh-autopair
+has nvim && config_neovim() {
+  if [[ ! -e ~/.config/nvim ]]; then
+    ln -s ~/.vim ~/.config/nvim
+    ln -s ~/.vimrc ~/.config/nvim/init.lua
+  fi
 }
 
-config_fzf() {
+has zsh && config_zsh() {
+  install_dots zsh zshrc zshenv zlogin
+  library https://github.com/zdharma/fast-syntax-highlighting ~/.zsh/plugins/fast-syntax-highlighting &
+  library https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/plugins/zsh-autosuggestions &
+  library https://github.com/hlissner/zsh-autopair ~/.zsh/plugins/zsh-autopair &
+  library https://github.com/MichaelAquilina/zsh-you-should-use ~/.zsh/plugins/you-should-use &
+  # library https://github.com/hchbaw/opp.zsh ~/.zsh/plugins/opp
+  wait
+}
+
+has fzf || config_fzf() {
   library https://github.com/junegunn/fzf.git ~/.fzf
   if [[ ! -x ~/.fzf/install ]]; then
     err 'failed to install fzf'
@@ -105,42 +125,41 @@ config_fzf() {
   ~/.fzf/install --all &> /dev/null
 }
 
-has -v tmux && config_tmux() {
-  backup_then_symlink tmux.conf
+has tmux && config_tmux() {
+  install_dots tmux.conf
   library https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
   [[ -x ~/.tmux/plugins/tpm/bin/install_plugins ]] && echo_cmd ~/.tmux/plugins/tpm/bin/install_plugins
 }
 
-has -v gem && config_gem() {
-  backup_then_symlink gemrc
+
+has gem && config_gem() {
+  install_dots gemrc
 }
 
-if has -v X; then
-  config_x11() {
-    has -v startx && backup_then_symlink xinitrc
-    has -v xmodmap && backup_then_symlink xmodmap
-    has -v xrdb && backup_then_symlink Xresources
-    if has -v fc-cache; then
-      [[ -f ~/.local/share/fonts/FantasqueSansMono-Regular.ttf ]] && return 0
-      mkdir -p ~/.local/share/fonts
-      if [[ ! -f ~/downloads/fantasque-sans-mono.zip ]]; then
-        echo_cmd curl -L --create-dirs -o ~/downloads/fantasque-sans-mono.zip \
-          'https://fontlibrary.org/assets/downloads/fantasque-sans-mono/db52617ba875d08cbd8e080ca3d9f756/fantasque-sans-mono.zip'
-      fi
-      if ! has -v unzip; then
-        err 'downloaded Fantasque Sans Mono but unzip is unavailable'
-        return 1
-      fi
-      if [[ -f ~/downloads/fantasque-sans-mono.zip ]]; then
-        echo_cmd unzip ~/downloads/fantasque-sans-mono.zip '*.ttf' -d ~/.local/share/fonts
-      else
-        err 'failed to download font Fantasque Sans Mono'
-      fi
+has X && config_x11() {
+  has -v startx && install_dots xinitrc
+  has -v xmodmap && install_dots xmodmap
+  has -v xrdb && install_dots Xresources
+  if has -v fc-cache; then
+    [[ -f ~/.local/share/fonts/FantasqueSansMono-Regular.ttf ]] && return 0
+    mkdir -p ~/.local/share/fonts
+    if [[ ! -f ~/downloads/fantasque-sans-mono.zip ]]; then
+      echo_cmd curl -L --create-dirs -o ~/downloads/fantasque-sans-mono.zip \
+        'https://fontlibrary.org/assets/downloads/fantasque-sans-mono/db52617ba875d08cbd8e080ca3d9f756/fantasque-sans-mono.zip'
     fi
-  }
-fi
+    if ! has -v unzip; then
+      err 'downloaded Fantasque Sans Mono but unzip is unavailable'
+      return 1
+    fi
+    if [[ -f ~/downloads/fantasque-sans-mono.zip ]]; then
+      echo_cmd unzip ~/downloads/fantasque-sans-mono.zip '*.ttf' -d ~/.local/share/fonts
+    else
+      err 'failed to download font Fantasque Sans Mono'
+    fi
+  fi
+}
 
-config_yarn() {
+has yarn || config_yarn() {
   echo_cmd curl -L https://yarnpkg.com/install.sh | sh
   ~/.yarn/bin/yarn config set init-version 0.0.1
   ~/.yarn/bin/yarn config set init-author-email DanielFGray@gmail.com
@@ -148,7 +167,7 @@ config_yarn() {
   ~/.yarn/bin/yarn config set init-license GPL-3.0
 }
 
-config_nvm() {
+has nvm || config_nvm() {
   echo_cmd curl -L https://raw.githubusercontent.com/creationix/nvm/master/install.sh | sh
 }
 
@@ -166,34 +185,35 @@ config() {
 }
 
 finish() {
-  printf '%s' "$(tput sgr0)"
+  tput sgr0
   exit
 }
 
 trap finish SIGHUP SIGINT SIGTERM
 
-while getopts ":hv" opt; do
+while getopts ":hvp:" opt; do
   case "$opt" in
     h) usage; exit ;;
     v) (( ++verbose )) ;;
+    p) process="$OPTARG" ;;
   esac
 done
 shift $(( OPTIND - 1 ))
 unset opt OPTARG OPTIND OPTERR
 
-has -v git curl || die 'git and curl both required'
-
 mapfile -t configs_avail < <(compgen -A function | awk '/^config_/{ sub(/^config_/, "", $0); print }')
 
-if in_term && [[ $1 != '--all' ]]; then
+if [[ $1 = '--all' ]]; then
+  configs_chosen=( "${configs_avail[@]}" )
+else
   printf 'will install the following groups:\n'
   read -r -e -p '> ' -i "${configs_avail[*]}" -a configs_chosen
-else
-  configs_chosen=( "${configs_avail[@]}" )
 fi
 
+(( configs_chosen > 0 )) || die
+
 for f in "${configs_chosen[@]}"; do
-  (( count++ >= 4 )) && wait -n
+  (( count++ >= process )) && wait -n
   config "$f" &
 done
 wait
